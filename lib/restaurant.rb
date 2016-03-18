c9c3a980c1a717be4b75e67cb9d4518718f72897
WolffDev <info@davidwolff.dk>
2016-03-18 12:11:10 +0100

    added listing method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -31,8 +31,15 @@ class Restaurant
   end
 
   def self.saved_restaurants
-    # read the restaurant file
-    # return instances of restaurant
+    restaurants = []
+    if file_usable?
+      file = File.new(@@filepath, 'r')
+      file.each_line do |line|
+        restaurants << Restaurant.new.import_line(line.chomp)
+      end
+      file.close
+    end
+    return restaurants
   end
 
   def initialize(args={})
@@ -53,6 +60,12 @@ class Restaurant
     args[:price] = gets.chomp.strip
 
     return self.new(args)
+  end
+
+  def import_line(line)
+    line_array = line.split("\t")
+    @name, @cuisine, @price = line_array
+    return self
   end
 
   def save
